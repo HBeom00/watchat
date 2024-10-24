644bdb2401337ae977e7049c6fef257795e5f4fc
play93 <95739833+play93@users.noreply.github.com>
2024-10-24 10:24:09 +0900

    fix : 병합 수정

--- src/app/(root)/(protected)/myPage/edit/page.tsx
+++ src/app/(root)/(protected)/myPage/edit/page.tsx
@@ -1,44 +1,44 @@
-"use client"
+'use client';
 
-import React, { useEffect, useRef, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
-import browserClient from '@/utils/supabase/client'
-import { useRouter } from 'next/navigation'
-import { getUserId } from '@/utils/getUserId'
-import Image from 'next/image'
-import { PostgrestError } from '@supabase/supabase-js'
+import React, { useEffect, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import browserClient from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+import { getUserId } from '@/utils/getUserId';
+import Image from 'next/image';
+import { PostgrestError } from '@supabase/supabase-js';
 
 const EditProfilePage = () => {
-  const router = useRouter()
-  const [imgFile, setImgFile] = useState("")
-  const [nickname, setNickname] = useState("")
+  const router = useRouter();
+  const [imgFile, setImgFile] = useState('');
+  const [nickname, setNickname] = useState('');
   const imgRef = useRef<HTMLInputElement>(null);
 
   type UserData = {
-    nickname:string;
-    profile_img:string;
-  } | null
+    nickname: string;
+    profile_img: string;
+  } | null;
 
   // 로그인한 사용자 정보를 불러옴 (기존 정보를 띄워주기 위해)
-  const fetchUserData = async () =>{
+  const fetchUserData = async () => {
     const userId = await getUserId();
-    const { data : userData, error }:{data : UserData, error : PostgrestError | null} = await browserClient
-    .from('user') 
-    .select('profile_img, nickname')
-    .eq('user_id', userId)
-    .single();
+    const { data: userData, error }: { data: UserData; error: PostgrestError | null } = await browserClient
+      .from('user')
+      .select('profile_img, nickname')
+      .eq('user_id', userId)
+      .single();
 
-    console.log("에러",error)
-    if(userData){
-      setImgFile(userData.profile_img)
-      setNickname(userData.nickname)
+    console.log('에러', error);
+    if (userData) {
+      setImgFile(userData.profile_img);
+      setNickname(userData.nickname);
     }
-    if(error){
-      console.error("사용자 정보를 불러오는데 실패했습니다. => ", error)
+    if (error) {
+      console.error('사용자 정보를 불러오는데 실패했습니다. => ', error);
     }
-  }
+  };
 
   // 페이지가 렌더링될 때 fetchUserData를 실행
   useEffect(() => {
@@ -47,65 +47,70 @@ const EditProfilePage = () => {
 
   // zod
   const editProfile = z.object({
-    nickname: z.string().min(2,"닉네임은 필수입니다.")
-  })
+    nickname: z.string().min(2, '닉네임은 필수입니다.')
+  });
 
   // 리액트 훅 폼 (유효성 검사)
-  const {register, handleSubmit, formState, watch} = useForm({
+  const { register, handleSubmit, formState, watch } = useForm({
     mode: 'onChange',
     defaultValues: {
-      nickname:nickname,
-
+      nickname: nickname
     },
     resolver: zodResolver(editProfile)
-  })
+  });
 
   //이미지 업로드 onChange
   const uploadImage = () => {
-    const file = imgRef.current?.files?.[0] // 선택한 파일을 file에 저장
-    if (file) { // file이 있다면?
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
+    const file = imgRef.current?.files?.[0]; // 선택한 파일을 file에 저장
+    if (file) {
+      // 파일이 있다면
+      const reader = new FileReader(); // 1. 사용할 FileReader를 reader에 선언
+      reader.readAsDataURL(file); // 2. .readAsDataURL메서드를 사용해 파일을 데이터 URL형식으로 변환
       reader.onloadend = () => {
-        if(typeof reader.result === 'string'){
-          setImgFile(reader.result)
+        // 3. FileReader가 파일을 다 읽고 난 뒤 실행되는 함수
+        if (typeof reader.result === 'string') {
+          setImgFile(reader.result); // 4. reader.result는 3에서 읽은 데이터를 담고있음. 이걸 ImgFile에 담아줌
         }
-      }
-      
+      };
     }
-  }
+  };
 
   //폼 제출 함수
   const onSubmit = async () => {
     const userId = await getUserId();
-    const {data: userData, error:updateError} = await browserClient
+    const { data: userData, error: updateError } = await browserClient
       .from('user')
       .update({
-        nickname:watch('nickname'),
+        nickname: watch('nickname'),
         profile_img: imgFile
       })
-      .eq('user_id',userId)
+      .eq('user_id', userId);
 
-    if(updateError) {
-      console.log('프로필 수정에 실패했습니다 => ', updateError)
-    }else {
-      console.log('프로필 수정에 성공했습니다 => ', userData)
+    if (updateError) {
+      console.log('프로필 수정에 실패했습니다 => ', updateError);
+    } else {
+      console.log('프로필 수정에 성공했습니다 => ', userData);
     }
 
-    router.push('/mypage')
-  }
+    router.push('/mypage');
+  };
 
   return (
     <section>
       <h1>프로필 수정</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Image src={imgFile || 'https://mdwnojdsfkldijvhtppn.supabase.co/storage/v1/object/public/profile_image/avatar.png'} alt="프로필 이미지" width={150} height={150}/>
-        <input id='selectImg' type='file' ref={imgRef} accept='image/*' onChange={uploadImage}/>
+        <Image
+          src={imgFile || 'https://mdwnojdsfkldijvhtppn.supabase.co/storage/v1/object/public/profile_image/avatar.png'}
+          alt="프로필 이미지"
+          width={150}
+          height={150}
+        />
+        <input id="selectImg" type="file" ref={imgRef} accept="image/*" onChange={uploadImage} />
 
         <div>
-          <label htmlFor='nickname'>닉네임</label>
-          <input {...register('nickname')} id="nickname" type='text' required/>
-          {formState.errors.nickname && (<span>{formState.errors.nickname.message}</span>)}
+          <label htmlFor="nickname">닉네임</label>
+          <input {...register('nickname')} id="nickname" type="text" required />
+          {formState.errors.nickname && <span>{formState.errors.nickname.message}</span>}
         </div>
         <div>
           <label>장르</label>
@@ -113,10 +118,10 @@ const EditProfilePage = () => {
         <div>
           <label>플랫폼</label>
         </div>
-        <button type='submit'>수정하기</button>
+        <button type="submit">수정하기</button>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default EditProfilePage
+export default EditProfilePage;
--- src/app/(root)/(protected)/myPage/page.tsx
+++ src/app/(root)/(protected)/myPage/page.tsx
@@ -1,36 +1,46 @@
-import React from "react";
+import React from 'react';
 
 const page = () => {
-  return <>
-  {/* 프로필 영역 */}
-  <section>
-      {/* <Image src={150} alt=""/> */}
-      <h3>닉네임</h3>
-      <button>팔로잉 255명</button>
-      <div>
+  return (
+    <>
+      {/* 프로필 영역 */}
+      <section>
+        {/* <Image src={150} alt=""/> */}
+        <h3>닉네임</h3>
+        <button>팔로잉 255명</button>
         <div>
-          <span>내 구독 채널</span>
-          <span></span>
+          <div>
+            <span>내 구독 채널</span>
+            <span></span>
+          </div>
+          <div>
+            <span>내 취향</span>
+            <span></span>
+          </div>
         </div>
         <div>
-          <span>내 취향</span>
-          <span></span>
+          <button>프로필 편집</button>
+          <button>파티 모집하기</button>
         </div>
-      </div>
-      <div>
-        <button>프로필 편집</button>
-        <button>파티 모집하기</button>
-      </div>
-  </section>
+      </section>
 
-  {/* 파티 및 파티원 영역  */}
-  <section>
-    <article><h3>참여한 파티</h3></article>
-    <article><h3>내가 오너인 파티</h3></article>
-    <article><h3>초대받은 파티</h3></article>
-    <article><h3>최근 함께했던 파티원</h3></article>
-  </section>
-  </>;
+      {/* 파티 및 파티원 영역  */}
+      <section>
+        <article>
+          <h3>참여한 파티</h3>
+        </article>
+        <article>
+          <h3>내가 오너인 파티</h3>
+        </article>
+        <article>
+          <h3>초대받은 파티</h3>
+        </article>
+        <article>
+          <h3>최근 함께했던 파티원</h3>
+        </article>
+      </section>
+    </>
+  );
 };
 
 export default page;
