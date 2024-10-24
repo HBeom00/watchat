30025a6c69bea95c36195f1389ac2828d6c541e2
Your Name <you@example.com>
2024-10-24 10:23:59 +0900

    pull 이전 commit

--- src/app/(root)/(protected)/participation/(components)/ParticipationForm.tsx
+++ src/app/(root)/(protected)/participation/(components)/ParticipationForm.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import browserClient from "@/utils/supabase/client";
+import browserClient from '@/utils/supabase/client';
 // import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState } from 'react';
 
-import React from "react";
+import React from 'react';
 
 const ParticipationForm = (partyId: { partyId: string }) => {
-  const [profile_image, setProfile_image] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [profile_image, setProfile_image] = useState('');
+  const [nickname, setNickname] = useState('');
   // const router = useRouter()
-  const userId = "로그인한 유저정보 들어갈 곳";
+  const userId = '로그인한 유저정보 들어갈 곳';
 
   // 참가하기 함수
   const submitHandler = async () => {
     const { data, error } = await browserClient
-      .from("team_user_profile")
+      .from('team_user_profile')
       .insert({ nickname, profile_image, party_id: partyId, user_id: userId });
     // if(error){
     //   throw new Error(error.message)
@@ -28,14 +28,8 @@ const ParticipationForm = (partyId: { partyId: string }) => {
   return (
     <>
       <div className="flex flex-col gap-10 p-10 bg-red-300">
-        <input
-          onChange={(e) => setProfile_image(e.target.value)}
-          placeholder="이미지를 입력하세요"
-        />
-        <input
-          onChange={(e) => setNickname(e.target.value)}
-          placeholder="닉네임을 입력하세요"
-        />
+        <input onChange={(e) => setProfile_image(e.target.value)} placeholder="이미지를 입력하세요" />
+        <input onChange={(e) => setNickname(e.target.value)} placeholder="닉네임을 입력하세요" />
       </div>
       <button onClick={submitHandler} className="bg-blue-300 rounded-2xl">
         참가하기!!
--- src/app/(root)/party/[id]/(components)/MemberList.tsx
+++ src/app/(root)/party/[id]/(components)/MemberList.tsx
@@ -1,13 +1,10 @@
-import { createClient } from "@/utils/supabase/server";
-import MemberProfileCard from "./MemberProfile";
+import { createClient } from '@/utils/supabase/server';
+import MemberProfileCard from './MemberProfile';
 
 const MemberList = async ({ partyNumber }: { partyNumber: string }) => {
   const supabase = createClient();
   // 파티 멤버 정보들을 불러오기
-  const { data } = await supabase
-    .from("team_user_profile")
-    .select("*")
-    .eq("party_id", partyNumber);
+  const { data } = await supabase.from('team_user_profile').select('*').eq('party_id', partyNumber);
 
   const isMember = true; //로그인한 사용자가 해당 파티에 가입했을 때;
   return (
@@ -19,7 +16,7 @@ const MemberList = async ({ partyNumber }: { partyNumber: string }) => {
             <MemberProfileCard
               key={mem.profile_id}
               partyNumber={partyNumber}
-              memberNumber={"멤버넘버"}
+              memberNumber={'멤버넘버'}
               isMember={isMember}
             />
           );
