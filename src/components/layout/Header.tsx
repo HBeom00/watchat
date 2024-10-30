932a9b3394faf9e5b830954ef71dae561e832224
김태흔 <you@example.com>
2024-10-30 15:12:14 +0900

    feat: 병합 커밋

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,20 @@ import Link from 'next/link';
 const Header = () => {
   const { isUser } = useUserStore((state) => state);
 
-  return <div>{isUser ? <LogoutButton /> : <Link href={'/login'}>로그인</Link>}</div>;
+  return( 
+<>
+    <h1>Watchat</h1>
+    <button>전체</button>
+    <button>시청중</button>
+    <button>모집중</button>
+    <input 
+    type="text"
+    placeholder='보고 싶은 콘텐츠를 검색해보세요'
+    />
+    <Link href={'/recruit/firstPage'}></Link>
+    <div>{isUser ? <LogoutButton /> : <Link href={'/login'}>로그인</Link>}</div>
+</>
+  )
 };
 
 export default Header;
