86cff9b5d0c2b1f7d4f5fd42aa80ceea53777233
play93 <95739833+play93@users.noreply.github.com>
2024-10-31 15:57:46 +0900

    feat : 더보기버튼과 연결된 각 페이지에 페이지네이션 추가 #55

--- src/app/(root)/(protected)/myPage/hosted-party/page.tsx
+++ src/app/(root)/(protected)/myPage/hosted-party/page.tsx
@@ -5,17 +5,26 @@ import { useFetchUserData } from '@/store/userStore';
 import { getViewStatus } from '@/utils/viewStatus';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 const MyOwnerParty = () => {
   // 사용자 데이터 가져오기
   const { data: userData, isPending, isError } = useFetchUserData();
   const userId = userData?.user_id;
 
-  // 주최중인 파티 가져오기
-  const { data: ownerParty, isPending: pandingOwnerParty, isError: errorOwnerParty } = useOwnerParty(userId as string);
+  // 페이지네이션 설정
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const pageSlice = 16;
+  const start = (pageNumber - 1) * pageSlice;
+  const end = start + pageSlice;
 
-  if (isPending || pandingOwnerParty) {
+  // 주최중인 파티 가져오기
+  const { data: ownerParty, isPending: pendingOwnerParty, isError: errorOwnerParty } = useOwnerParty(userId as string);
+
+  // 페이지 수 불러오기
+  const pageCount = ownerParty ? Math.ceil(ownerParty.length / pageSlice) : 1;
+
+  if (isPending || pendingOwnerParty) {
     return <div>사용자 정보를 불러오는 중 입니다...</div>;
   }
   if (isError || errorOwnerParty) {
@@ -27,7 +36,7 @@ const MyOwnerParty = () => {
       <h3>내가 오너인 파티</h3>
       <ul>
         {ownerParty && ownerParty.length > 0 ? (
-          ownerParty.map((party) => {
+          ownerParty.slice(start, end).map((party) => {
             const viewingStatus = getViewStatus(party); // 시청 상태
 
             return (
@@ -78,6 +87,19 @@ const MyOwnerParty = () => {
           <li>주최한 파티가 없습니다.</li>
         )}
       </ul>
+      <div className="flex flex-row gap-10 p-10 justify-center items-center text-xl font-bold">
+        {Array.from({ length: pageCount })
+          .map((arr, i) => {
+            return i + 1;
+          })
+          .map((page) => {
+            return (
+              <button key={page} onClick={() => setPageNumber(page)}>
+                {page}
+              </button>
+            );
+          })}
+      </div>
     </article>
   );
 };
--- src/app/(root)/(protected)/myPage/participating-party/page.tsx
+++ src/app/(root)/(protected)/myPage/participating-party/page.tsx
@@ -5,11 +5,18 @@ import { useFetchUserData } from '@/store/userStore';
 import { getViewStatus } from '@/utils/viewStatus';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 const ParticipatingParty = () => {
+  // 사용자 데이터 가져오기
   const { data: userData, isPending, isError } = useFetchUserData();
   const userId = userData?.user_id;
+
+  // 페이지네이션 설정
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const pageSlice = 16;
+  const start = (pageNumber - 1) * pageSlice;
+  const end = start + pageSlice;
 
   // 참여중인 파티 가져오기
   const {
@@ -17,6 +24,9 @@ const ParticipatingParty = () => {
     isPending: pendingEnjoyingParty,
     isError: errorEnjoyingParty
   } = useParticipatingParty(userId as string);
+
+  // 페이지 수 불러오기
+  const pageCount = enjoyingParty ? Math.ceil(enjoyingParty.length / pageSlice) : 1;
 
   if (isPending || pendingEnjoyingParty) {
     return <div>사용자 정보를 불러오는 중 입니다...</div>;
@@ -30,7 +40,7 @@ const ParticipatingParty = () => {
       <h3>참여한 파티</h3>
       <ul>
         {enjoyingParty && enjoyingParty.length > 0 ? (
-          enjoyingParty.map((party) => {
+          enjoyingParty.slice(start, end).map((party) => {
             const viewingStatus = getViewStatus(party); // 시청 상태
 
             return (
@@ -83,6 +93,19 @@ const ParticipatingParty = () => {
           <li>참가중인 파티가 없습니다.</li>
         )}
       </ul>
+      <div className="flex flex-row gap-10 p-10 justify-center items-center text-xl font-bold">
+        {Array.from({ length: pageCount })
+          .map((arr, i) => {
+            return i + 1;
+          })
+          .map((page) => {
+            return (
+              <button key={page} onClick={() => setPageNumber(page)}>
+                {page}
+              </button>
+            );
+          })}
+      </div>
     </section>
   );
 };
