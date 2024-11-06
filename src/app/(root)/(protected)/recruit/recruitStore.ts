38eb2c6d520b313accfb27a57cc8a984558a2b47
김태흔 <you@example.com>
2024-11-07 08:41:33 +0900

    feat : 저장안된데이터

--- src/app/(root)/(protected)/recruit/recruitStore.ts
+++ src/app/(root)/(protected)/recruit/recruitStore.ts
@@ -2,47 +2,50 @@
 import { create } from 'zustand';
 
 interface RecruitState {
-    party_name: string;
-    video_name: string;
-    party_detail: string;
-    limited_member: number;
-    watch_date: Date | null;
-    start_time: Date | null;
-    duration_time: number;
-    video_platform: { name: string; logoUrl: string }[] | null;
-    media_type: string;
-    video_image: string;
-    backdrop_image: string;
-    video_id: number | null;
-    episode_number: number | null;
-    situation: string;
-    popularity: number | null;
+  party_name: string;
+  video_name: string;
+  party_detail: string;
+  limited_member: number;
+  watch_date: Date | null;
+  start_time: Date | null;
+  duration_time: number;
+  video_platform: { name: string; logoUrl: string }[] | null;
+  media_type: string;
+  video_image: string;
+  backdrop_image: string;
+  video_id: number | null;
+  episode_number: number | null;
+  situation: string;
+  popularity: number | null;
+  date_recruitment: Date | null;
+  season_number: number | null;
 
-    setPartyInfo: (info: Partial<Omit<RecruitState, "setPartyInfo" | "setRecruitDetails">>) => void;
-    setRecruitDetails: (details: Partial<Omit<RecruitState, "setPartyInfo" | "setRecruitDetails">>) => void;
+  setPartyInfo: (info: Partial<Omit<RecruitState, 'setPartyInfo' | 'setRecruitDetails'>>) => void;
+  setRecruitDetails: (details: Partial<Omit<RecruitState, 'setPartyInfo' | 'setRecruitDetails'>>) => void;
 }
 
 export const useRecruitStore = create<RecruitState>((set) => ({
-    party_name: '',
-    video_name: '',
-    party_detail: '',
-    limited_member: 0,
-    watch_date: null,
-    start_time: null,
-    duration_time: 0,
-    video_platform: [],
-    media_type: '',
-    video_image: '',
-    backdrop_image:'',
-    video_id: null,
-    episode_number: null,
-    situation: "모집중",
-    popularity: null,
+  party_name: '',
+  video_name: '',
+  party_detail: '',
+  limited_member: 0,
+  watch_date: null,
+  start_time: null,
+  duration_time: 0,
+  video_platform: [],
+  media_type: '',
+  video_image: '',
+  backdrop_image: '',
+  video_id: null,
+  episode_number: null,
+  situation: '모집중',
+  popularity: null,
+  date_recruitment: null,
+  season_number: null,
 
-    // 아래 애들이 요청에 들어감
-    setPartyInfo: (info) => set((state) => ({ ...state, ...info })),
-    setRecruitDetails: (details) => set((state) => ({ ...state, ...details })),
+  // 아래 애들이 요청에 들어감
+  setPartyInfo: (info) => set((state) => ({ ...state, ...info })),
+  setRecruitDetails: (details) => set((state) => ({ ...state, ...details }))
 }));
-
 
 // 쿼리스트링으로 저장 해두면 새로고침해도 데이터 안날라감
