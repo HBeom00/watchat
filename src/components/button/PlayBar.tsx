7dd61a1388be94dbc828b54d3e8145b21ee5c0b1
Hbeom00 <bum6968@naver.com>
2024-11-13 10:19:59 +0900

    playbar 수정 완료

--- src/components/button/PlayBar.tsx
+++ src/components/button/PlayBar.tsx
@@ -1,20 +1,28 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const PlayBar = ({ startTime, duration }: { startTime: string; duration: number }) => {
   const startPlayTime = startTime;
-
   const movie_hour = duration / 60;
   const movie_second = duration % 60;
-
   const playEnd = [Math.floor(movie_hour), movie_second < 10 ? `0${movie_second}` : movie_second];
-
   const [playTime, setPlayTime] = useState<number>(0);
+
+  const updateSliderBackground = () => {
+    const slider = document.getElementById('playRange') as HTMLInputElement;
+    const progress = (playTime / (duration * 60)) * 100; // 총 지속 시간에 대한 현재 재생 시간의 백분율 계산
+    slider.style.background = `linear-gradient(to right, #7f4af4 ${progress}%, #dcdcdc ${progress}%)`;
+  };
+
+  useEffect(() => {
+    updateSliderBackground();
+  }, [playTime]);
 
   if (duration * 60 > playTime) {
     setTimeout(() => setPlayTime(getPlayTime(startPlayTime)), 1000);
   }
+
   return (
     <div className="pt-2 pb-4 px-4 flex flex-col items-start gap-2 self-stretch bg-white">
       <div className="flex justify-between items-center self-stretch">
@@ -27,6 +35,7 @@ const PlayBar = ({ startTime, duration }: { startTime: string; duration: number 
       </div>
       <input
         type="range"
+        id="playRange" // id를 추가하여 스타일링할 수 있게 함
         min={0}
         max={duration * 60}
         step={1}
@@ -41,11 +50,9 @@ const PlayBar = ({ startTime, duration }: { startTime: string; duration: number 
 export default PlayBar;
 
 const getPlayTime = (startTime: string) => {
-  // 시작 시각
   const startTimeArr = startTime.split('.')[0].split('T')[1].split(':').map(Number);
   const startPlayTime = startTimeArr[0] * 60 * 60 + startTimeArr[1] * 60 + startTimeArr[2];
 
-  // 현재 시각
   const myTime = new Date();
   myTime.setHours(myTime.getHours() + 9);
   const localTime = myTime.toISOString();
@@ -53,8 +60,6 @@ const getPlayTime = (startTime: string) => {
 
   let nowTime = localTimeArr[0] * 60 * 60 + localTimeArr[1] * 60 + localTimeArr[2];
 
-  // 영상을 보다가 자정00:00이 넘는 경우를 생각함
-  // 8시 이전 로직
   if (nowTime < 8 * 60 * 60 && !(startPlayTime < 8 * 60 * 60)) {
     nowTime += 24 * 60 * 60;
   }
@@ -62,7 +67,6 @@ const getPlayTime = (startTime: string) => {
   return nowTime - startPlayTime;
 };
 
-// 현재 영상진도
 const nowTimeDisplay = (playTime: number) => {
   const movie_now_hour = playTime / 60 / 60;
   const movie_now_minute = playTime / 60;
