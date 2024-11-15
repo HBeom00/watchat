1607b2f5ea2d884f8b0129076d076b2f67669180
Hbeom00 <bum6968@naver.com>
2024-11-15 11:41:43 +0900

    사이드바 코드 수정

--- src/utils/hooks/useLiveSubscribe.ts
+++ src/utils/hooks/useLiveSubscribe.ts
@@ -11,18 +11,14 @@ export const useLiveSubscribe = (roomId: string, queryClient: QueryClient) => {
   // 실시간 구독 설정
   useEffect(() => {
     const channel: RealtimeChannel = browserClient
-      .channel(`chat-${roomId}`)
-      .on(
-        'postgres_changes',
-        { event: 'INSERT', schema: 'public', table: 'team_user_profile', filter: `party_id=eq.${roomId}` },
-        (payload) => {
-          console.log(payload, 'payload');
-          queryClient.setQueryData<UserInfo[]>(['members', roomId], (oldMembers = []) => [
-            ...oldMembers,
-            payload.new as UserInfo
-          ]);
-        }
-      )
+      .channel(`team_user_profile`)
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'team_user_profile' }, (payload) => {
+        console.log(payload, 'payload');
+        queryClient.setQueryData<UserInfo[]>(['members', roomId], (oldMembers = []) => [
+          ...oldMembers,
+          payload.new as UserInfo
+        ]);
+      })
       .on('postgres_changes', { event: 'DELETE', schema: 'public', table: 'team_user_profile' }, (payload) => {
         queryClient.setQueryData<UserInfo[]>(queryKey.chat.members(roomId), (oldMembers = []) =>
           oldMembers.filter((member) => member.profile_id !== payload.old.profile_id)
