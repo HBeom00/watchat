72f00e9a00056ba498431fb015558229bfc628f2
play93 <95739833+play93@users.noreply.github.com>
2024-11-16 16:10:42 +0900

    feat : 식빵온도 코멘트 가져오기

--- src/utils/myPage/getWarming.ts
+++ src/utils/myPage/getWarming.ts
@@ -1,7 +1,7 @@
 import browserClient from '@/utils/supabase/client';
 import { useQuery } from '@tanstack/react-query';
 
-// warming 테이블에서 해당 user_id와 일치하는 모든 temperature 값을 합산
+// 식빵온도 가져오기
 export const getWarming = async (userId: string | undefined): Promise<number> => {
   // warming 테이블에서 해당 user_id와 일치하는 warming_user_id의 temperature 합산
   const { data, error } = await browserClient.from('warming').select('temperature').eq('warming_user_id', userId);
@@ -21,9 +21,60 @@ export const getWarming = async (userId: string | undefined): Promise<number> =>
   return totalTemperature ?? 50; // 데이터가 없다면 기본값 50 반환
 };
 
+// 코멘트 가져오기
+export const getComments = async (userId: string | undefined) => {
+  const { data: commentData, error: commentError } = await browserClient
+    .from('warming')
+    .select('comment')
+    .eq('warming_user_id', userId);
+
+  if (commentError) {
+    console.error('comment 데이터 불러오기에 실패했습니다', commentError.message);
+    return [];
+  }
+
+  const commentsList = [
+    '매너있게 관람했어요.',
+    '시간 약속을 잘 지켜요.',
+    '다음에 같이 시청하고 싶어요.',
+    '시청 매너가 좋아요.',
+    '즐겁게 관람했어요.',
+    '시청 매너가 아쉬웠어요.',
+    '시간 약속을 지키지 않아요.',
+    '다음에 함께하고 싶지 않아요.',
+    '참여하지 않았어요.',
+    '중간에 나가서 돌아오지 않았어요.'
+  ];
+
+  const commentCounts: { comment: string; commentCount: number }[] = commentsList.map((comment) => ({
+    comment,
+    commentCount: 0
+  }));
+
+  // 각 코멘트의 갯수구하기
+  commentData?.forEach((entry: { comment: string[] }) => {
+    entry.comment.forEach((singleComment) => {
+      const findSameComments = commentCounts.find((c) => c.comment === singleComment);
+
+      if (findSameComments) {
+        findSameComments.commentCount++;
+      }
+    });
+  });
+
+  return commentCounts.filter((c) => c.commentCount > 0);
+};
+
 export const useTotalTemperature = (userId: string) => {
   return useQuery({
     queryKey: ['warming', userId],
     queryFn: () => getWarming(userId)
+  });
+};
+
+export const useTotalComments = (userId: string) => {
+  return useQuery({
+    queryKey: ['comments', userId],
+    queryFn: () => getComments(userId)
   });
 };
