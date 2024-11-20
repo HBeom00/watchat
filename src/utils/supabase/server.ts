698c57f010fd5b064fd5c20491bf21d4ba1234be
Hbeom00 <bum6968@naver.com>
2024-11-20 14:42:01 +0900

    utils/supabase 로직 복구

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -36,6 +36,12 @@ export const getLoginUserIdOnServer = async () => {
     error
   } = await createClient().auth.getUser();
 
+  // 로그인 상태 확인
+  if (!user) {
+    console.error('로그인정보가 없습니다.');
+    return null; // 사용자 정보가 없으면 null 반환
+  }
+
   if (error) {
     console.error('로그인한 유저 정보를 가져오지 못 했습니다. => ', error);
     return null;
